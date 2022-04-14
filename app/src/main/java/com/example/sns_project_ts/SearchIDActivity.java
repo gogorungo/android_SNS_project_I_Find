cf594935704cc84f27f3e9b3ad421dc44e35802f
gogorungo <tkxozlf@gmail.com>
2022-04-14 17:15:33 +0900

    아이디찾기 거의 완성

--- app/src/main/java/com/example/sns_project_ts/SearchIDActivity.java
+++ app/src/main/java/com/example/sns_project_ts/SearchIDActivity.java
@@ -2,10 +2,13 @@ package com.example.sns_project_ts;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.telephony.SmsManager;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,13 +17,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
@@ -40,8 +40,9 @@ public class SearchIDActivity extends AppCompatActivity {
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            int id = v.getId();
 
-            if(v.getId() == R.id.checkIDButton){
+            if(id == R.id.checkIDButton){
 
                 String number = ((EditText) findViewById(R.id.searchID)).getText().toString();
 
@@ -62,6 +63,8 @@ public class SearchIDActivity extends AppCompatActivity {
             }
         }
     };
+
+
 
     private void searchId(String number){
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -103,7 +106,11 @@ public class SearchIDActivity extends AppCompatActivity {
     }
 
     private void sendSMS(String email, String number){
-        View dialogView = (View) View.inflate(SearchIDActivity.this,R.layout.search_id_dialog,null);
+
+        View dialogView = (View) View.inflate(SearchIDActivity.this,R.layout.dialog_search_id,null);
+        ProgressBar timerBar = (ProgressBar) dialogView.findViewById(R.id.timerBar);
+        TextView timerNumber = (TextView) dialogView.findViewById(R.id.timerNumber);
+
         AlertDialog.Builder dlg = new AlertDialog.Builder(this)
                 .setTitle("코드 입력")
                 .setView(dialogView)
@@ -113,6 +120,80 @@ public class SearchIDActivity extends AppCompatActivity {
                         startToast("취소했습니다");
                     }
                 });
-        dlg.show();
+
+        AlertDialog aDialog = dlg.create();
+
+        int randomCode = (int) (1 + Math.random() * 999999);
+        Log.d(TAG, "codeNumber1 "+ randomCode);
+
+        EditText codeText = (EditText) dialogView.findViewById(R.id.inputCode);
+        Button codeBtn = (Button) dialogView.findViewById(R.id.checkCodeBtn);
+
+        codeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String codeA = codeText.getText().toString();
+                String codeQ = String.valueOf(randomCode);
+
+                if(codeA.equals(codeQ)){
+                    aDialog.cancel();
+                    conFirmID(email);
+
+
+                }else{
+                    startToast("코드가 다릅니다");
+                }
+            }
+        });
+
+
+        Thread th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                for (int i = timerBar.getProgress() ; i > 0 ; i--) {
+                    timerBar.setProgress(timerBar.getProgress() - 1);
+                    timerNumber.setText("남은 시간 : " + timerBar.getProgress());
+                    SystemClock.sleep(1000);
+                }
+                SearchIDActivity.this.runOnUiThread(new Runnable() {
+                    public void run() {
+                        Toast.makeText(SearchIDActivity.this, "시간이 초과되었습니다", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                aDialog.cancel();
+            }
+        });
+
+        th.setDaemon(true);
+        th.start();
+
+        aDialog.show();
     }
+
+
+
+
+    private void conFirmID(String email){
+
+        AlertDialog.Builder dlg = new AlertDialog.Builder(this)
+                .setTitle("아이디 확인")
+                .setMessage(email)
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startToast("확인되었습니다");
+                        finish();
+                    }
+                });
+
+        AlertDialog alertDialog = dlg.show();
+        TextView messageText = (TextView)alertDialog.findViewById(android.R.id.message);
+        messageText.setTextSize(25);
+        alertDialog.show();
+
+    }
+
+
 }
